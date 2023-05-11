11ba482a69e37e6b3d388fd3a554ebfef36266ac
JulianFreu <julian.guenter93@gmail.com>
2023-05-11 22:10:34 +0200

    code done, not simulated yet

--- TMDS_8b10b_encoder.vhd
+++ TMDS_8b10b_encoder.vhd
@@ -17,37 +17,47 @@ end TMDS_8b10b_encoder;
 architecture rtl of TMDS_8b10b_encoder is
     component count_ones is 
         generic (
-            g_VECTOR_LENGTH := 8
+            g_VECTOR_LENGTH : integer
         );
         port (
-            i_data          : std_logic_vector(g_VECTOR_LENGTH-1 downto 0);
-            o_num_of_ones   : std_logic;
+            i_data          : in std_logic_vector(g_VECTOR_LENGTH-1 downto 0);
+            o_num_of_ones   : out integer range 0 to g_VECTOR_LENGTH
+        );
+    end component;
+
+    signal r_cnt_disparity      : integer range -8 to 8 := 0;
+    signal w_int_data_9b        : std_logic_vector(8 downto 0);
+    signal w_int_data_10b        : std_logic_vector(9 downto 0);
+    signal w_ones_i_data       : integer range 0 to 8 := 0;
+    signal w_ones_int_data       : integer range 0 to 9 := 0;
+
+begin
+
+    count_ones_8b : count_ones
+        generic map (
+            g_VECTOR_LENGTH => 8
+        )
+        port map ( 
+            i_data => i_data,
+            o_num_of_ones => w_ones_i_data
+        );
+
+    count_ones_9b : count_ones
+        generic map (
+            g_VECTOR_LENGTH => 9
+        )
+        port map ( 
+            i_data => w_int_data_9b,
+            o_num_of_ones => w_ones_int_data
         );
 
 
-    signal r_cnt_disparity      : integer range -8 to 8 := 0;
-    signal w_int_data_9b           : std_logic_vector(8 downto 0);
-    signal w_ones               : integer range 0 to 8 := 0;
-    signal w_zeroes             : integer range 0 to 8 := 0;
-
-begin
-
--- counts how many 1s are in the input byte
-count_ones : process(i_data)
-signal count : integer range 0 to 8 := 0;
-begin
-    for i in 0 to 7 : loop
-        if (i_data(i) = '1') then
-            count := count+1;
-        end if;
-    end loop;
-    w_ones := count;
-end process;
+o_data <= w_int_data_10b;
 
 encode : process(i_data)
     signal w_temp : std_logic_vector(7 downto 0);
 begin
-    if (w_ones >= 4) then                               -- when there are 4 or more 1s, the bits get xnor'ed
+    if (w_ones_i_data >= 4) then                               -- when there are 4 or more 1s, the bits get xnor'ed
         for i in 0 to 6 loop
             w_temp(i) <= i_data(i) xnor i_data(i+1);
         end loop;
@@ -63,26 +73,48 @@ end process;
 
 invert : process(w_int_data_9b)
 begin
-    if (r_cnt_disparity = 0 or w_ones = 4) then
-        o_data(9) <= not w_int_data_9b(8);
-        o_data(8) <= w_int_data_9b(8);
+    if (r_cnt_disparity = 0 or w_ones_int_data = 4) then
+        w_int_data_10b(9) <= not w_int_data_9b(8);
+        w_int_data_10b(8) <= w_int_data_9b(8);
         if (w_int_data_9b(8) = '1') then
-            o_data(7 downto 0) <= not w_int_data_9b(7 downto 0);
+            w_int_data_10b(7 downto 0) <= w_int_data_9b(7 downto 0);
         else
-            o_data(7 downto 0) <= w_int_data_9b(7 downto 0);
+            w_int_data_10b(7 downto 0) <= not w_int_data_9b(7 downto 0);
         end if;        
-    elsif disparity worsens
-        invert
-        set bit 10 to 1
+    elsif ((r_cnt_disparity > 0 and w_ones_int_data > 8 - w_ones_int_data) or (r_cnt_disparity < 0 and (8-w_ones_int_data > w_ones_int_data))) then
+        w_int_data_10b <= '1' & w_int_data_9b(8) & not w_int_data_9b(7 downto 0);
     else
-        dont invert
-        set bit 10 to 0 
+        w_int_data_10b <= '0' & w_int_data_9b(8 downto 0);
+    end if;
 end process;
 
-process(i_clk)
+set_output : process(i_clk)
 
 begin
-
+    if (rising_edge(i_clk)) then
+        if (i_data_enable = '0') then
+            r_cnt_disparity <= 0;
+            w_int_data_10b <=   "0010101011" when (i_C0 = '0' and i_C1 = '0') else
+                                "1101010100" when (i_C0 = '1' and i_C1 = '0') else
+                                "0010101010" when (i_C0 = '0' and i_C1 = '1') else
+                                "1101010101" when (i_C0 = '1' and i_C1 = '1') else
+                                (others => '0');
+        elsif (r_cnt_disparity = 0 or w_ones_int_data = 4) then
+            r_cnt_disparity <=  (r_cnt_disparity + w_ones_int_data - (8 - w_ones_int_data)) when w_int_data_9b(8) = '0' else
+                                (r_cnt_disparity + (8 - 2*w_ones_int_data)) when w_int_data_9b(8) = '1' else
+                                0;
+        else
+            if w_int_data_10b(9) = '1' and w_int_data_10b(8) = '1' then
+                r_cnt_disparity <= r_cnt_disparity +2 + (8 - 2*w_ones_int_data);
+            elsif w_int_data_10b(9) = '1' and w_int_data_10b(8) = '0' then
+                r_cnt_disparity <= r_cnt_disparity + (8 - 2*w_ones_int_data);
+            elsif w_int_data_10b(9) = '0' and w_int_data_10b(8) = '1' then
+                r_cnt_disparity <= r_cnt_disparity + w_ones_int_data -(8-w_ones_int_data);
+            else 
+                r_cnt_disparity <= r_cnt_disparity -2 + (w_ones_int_data -(8-w_ones_int_data));
+            end if;
+        end if;
+    end if;
 end process;
 
 end architecture;
