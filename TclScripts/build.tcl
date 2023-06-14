135099423d46fe358e00c38dea469672d255e83e
JulianFreu <julian.guenter93@gmail.com>
2023-06-14 21:13:56 +0200

    changed pixel clock to 40MHz for 800x600@60fps

--- TclScripts/build.tcl
+++ TclScripts/build.tcl
@@ -22,35 +22,31 @@ if { [file isdirectory ../"IP"] } {
         -dir ../IP
 
     set_property -dict [list \
-        CONFIG.CLKIN1_JITTER_PS {80.0} \
         CONFIG.CLKOUT1_DRIVES {BUFG} \
-        CONFIG.CLKOUT1_JITTER {138.255} \
-        CONFIG.CLKOUT1_PHASE_ERROR {222.305} \
-        CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {600} \
+        CONFIG.CLKOUT1_JITTER {139.033} \
+        CONFIG.CLKOUT1_PHASE_ERROR {87.180} \
+        CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {40} \
         CONFIG.CLKOUT2_DRIVES {BUFG} \
-        CONFIG.CLKOUT2_JITTER {191.470} \
-        CONFIG.CLKOUT2_PHASE_ERROR {222.305} \
-        CONFIG.CLKOUT2_REQUESTED_OUT_FREQ {60} \
+        CONFIG.CLKOUT2_JITTER {90.074} \
+        CONFIG.CLKOUT2_PHASE_ERROR {87.180} \
+        CONFIG.CLKOUT2_REQUESTED_OUT_FREQ {400} \
         CONFIG.CLKOUT2_USED {true} \
         CONFIG.CLKOUT3_DRIVES {BUFG} \
         CONFIG.CLKOUT4_DRIVES {BUFG} \
         CONFIG.CLKOUT5_DRIVES {BUFG} \
         CONFIG.CLKOUT6_DRIVES {BUFG} \
         CONFIG.CLKOUT7_DRIVES {BUFG} \
-        CONFIG.CLK_IN1_BOARD_INTERFACE {Custom} \
-        CONFIG.CLK_OUT1_PORT {clk_600MHz} \
-        CONFIG.CLK_OUT2_PORT {clk_60MHz} \
+        CONFIG.CLK_OUT1_PORT {clk_40MHz} \
+        CONFIG.CLK_OUT2_PORT {clk_400MHz} \
+        CONFIG.Component_Name {pll_1} \
         CONFIG.MMCM_BANDWIDTH {OPTIMIZED} \
-        CONFIG.MMCM_CLKFBOUT_MULT_F {48} \
-        CONFIG.MMCM_CLKIN1_PERIOD {8.000} \
-        CONFIG.MMCM_CLKIN2_PERIOD {10.000} \
-        CONFIG.MMCM_CLKOUT0_DIVIDE_F {2} \
-        CONFIG.MMCM_CLKOUT1_DIVIDE {20} \
+        CONFIG.MMCM_CLKFBOUT_MULT_F {12} \
+        CONFIG.MMCM_CLKOUT0_DIVIDE_F {30} \
+        CONFIG.MMCM_CLKOUT1_DIVIDE {3} \
         CONFIG.MMCM_COMPENSATION {ZHOLD} \
-        CONFIG.MMCM_DIVCLK_DIVIDE {5} \
+        CONFIG.MMCM_DIVCLK_DIVIDE {1} \
         CONFIG.NUM_OUT_CLKS {2} \
         CONFIG.PRIMITIVE {PLL} \
-        CONFIG.PRIM_IN_FREQ {125.000} \
     ] [get_ips pll_1]
 
     generate_target all [get_ips]
@@ -67,7 +63,6 @@ read_vhdl ../RTL/count_ones.vhd
 read_vhdl ../RTL/TMDS_8b10b_encoder.vhd
 read_vhdl ../RTL/TMDS_decoder.vhd
 read_xdc ../Constraints/PYNQ_Z2_v1.xdc
-
 #
 # STEP#3: run synthesis, write design checkpoint, report timing, 
 # and utilization estimates
@@ -87,15 +82,17 @@ opt_design
 # reportCriticalPaths $outputDir/post_opt_critpath_report.csv #this is a custom command
 place_design
 report_clock_utilization -file $outputDir/clock_util.rpt
+report_timing_summary -file $outputDir/post_place_timing_summary.rpt
+phys_opt_design
 #
 # Optionally run optimization if there are timing violations after placement
-if {[get_property SLACK [get_timing_paths -max_paths 1 -nworst 1 -setup]] < 0} {
-    puts "Found setup timing violations => running physical optimization"
-    phys_opt_design
-}
+#if {[get_property SLACK [get_timing_paths -max_paths 1 -nworst 1 -setup]] < 0} {
+#    puts "Found setup timing violations => running physical optimization"
+#    phys_opt_design
+#}
 write_checkpoint -force $outputDir/post_place.dcp
 report_utilization -file $outputDir/post_place_util.rpt
-report_timing_summary -file $outputDir/post_place_timing_summary.rpt
+report_timing_summary -file $outputDir/post_place_opt_timing_summary.rpt
 #
 # STEP#5: run the router, write the post-route design checkpoint, report the routing
 # status, report timing, power, and DRC, and finally save the Verilog netlist.
@@ -104,7 +101,9 @@ route_design
 write_checkpoint -force $outputDir/post_route.dcp
 report_route_status -file $outputDir/post_route_status.rpt
 report_timing_summary -file $outputDir/post_route_timing_summary.rpt
+phys_opt_design
 report_power -file $outputDir/post_route_power.rpt
+report_timing_summary -file $outputDir/post_route_opt_timing_summary.rpt
 report_drc -file $outputDir/post_imp_drc.rpt
 # write_verilog -force $outputDir/cpu_impl_netlist.v -mode timesim -sdf_anno true
 #
